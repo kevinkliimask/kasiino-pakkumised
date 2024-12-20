b6a516cea65d0d6a18ddf8e2486ce0edd41df988
luckasw <albert.wihler@gmail.com>
2024-12-20 01:32:50 +0100

    feat: add bonus used message to article card

--- components/article-card.tsx
+++ components/article-card.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { Link } from 'next-view-transitions';
 
@@ -7,22 +10,47 @@ type ArticleCardProps = {
   endDate: Date | null;
 };
 
-const ArticleCard = async ({ title, slug, endDate }: ArticleCardProps) => {
-  const image = (await import(`@/app/pakkumine/${slug}/header.webp`)).default;
+const ArticleCard = ({ title, slug, endDate }: ArticleCardProps) => {
+  const [isBonusUsed, setIsBonusUsed] = useState(false);
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    const bonusUsed = localStorage.getItem(`bonus-used-${slug}`);
+    if (bonusUsed === 'true') {
+      setIsBonusUsed(true);
+    }
+  }, [slug]);
+
+  useEffect(() => {
+    const loadImage = async () => {
+      const importedImage = (
+        await import(`@/app/pakkumine/${slug}/header.webp`)
+      ).default;
+      setImage(importedImage);
+    };
+    loadImage();
+  }, [slug]);
 
   return (
     <Link href={`/pakkumine/${slug}`}>
       <div className="flex flex-col h-full shadow-sm rounded-[10px] overflow-hidden hover:shadow-lg group hover:bg-accent transition-all">
         <div className="relative w-full aspect-[16/9]">
-          <Image
-            src={image}
-            alt={title}
-            fill
-            className="object-cover group-hover:grayscale transition-all"
-          />
+          {image && (
+            <Image
+              src={image}
+              alt={title}
+              fill
+              className="object-cover group-hover:grayscale transition-all"
+            />
+          )}
         </div>
         <div className="flex flex-col gap-2 p-4 flex-grow">
           <h2 className="text-lg font-semibold">{title}</h2>
+          {isBonusUsed && (
+            <span className="text-sm text-green-500 font-bold">
+              Boonus kasutatud
+            </span>
+          )}
           {endDate ? (
             <p className="text-sm text-muted-foreground">
               Kehtib kuni{' '}
