a39d19c35f9e50c5564e4c394da158ba1ca4a422
kevinkliimask <kevinkliimask@gmail.com>
2024-12-26 22:42:39 +0200

    add prettier & format

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -9,30 +9,16 @@ type AnchorProps = ComponentPropsWithoutRef<'a'>;
 type BlockquoteProps = ComponentPropsWithoutRef<'blockquote'>;
 
 const components = {
-  h1: (props: HeadingProps) => (
-    <h1 className="text-3xl font-bold mb-8" {...props} />
-  ),
-  h2: (props: HeadingProps) => (
-    <h2 className="text-xl font-semibold mb-4" {...props} />
-  ),
-  h3: (props: HeadingProps) => (
-    <h3 className="text-lg font-medium mb-2" {...props} />
-  ),
+  h1: (props: HeadingProps) => <h1 className="text-3xl font-bold mb-8" {...props} />,
+  h2: (props: HeadingProps) => <h2 className="text-xl font-semibold mb-4" {...props} />,
+  h3: (props: HeadingProps) => <h3 className="text-lg font-medium mb-2" {...props} />,
   h4: (props: HeadingProps) => <h4 className="font-medium" {...props} />,
   p: (props: ParagraphProps) => <p className="mb-4" {...props} />,
-  ol: (props: ListProps) => (
-    <ol className="list-decimal pl-5 space-y-2 mb-4" {...props} />
-  ),
-  ul: (props: ListProps) => (
-    <ul className="list-disc pl-5 space-y-1 mb-4" {...props} />
-  ),
+  ol: (props: ListProps) => <ol className="list-decimal pl-5 space-y-2 mb-4" {...props} />,
+  ul: (props: ListProps) => <ul className="list-disc pl-5 space-y-1 mb-4" {...props} />,
   li: (props: ListItemProps) => <li className="pl-1" {...props} />,
-  em: (props: ComponentPropsWithoutRef<'em'>) => (
-    <em className="font-medium" {...props} />
-  ),
-  strong: (props: ComponentPropsWithoutRef<'strong'>) => (
-    <strong className="font-bold" {...props} />
-  ),
+  em: (props: ComponentPropsWithoutRef<'em'>) => <em className="font-medium" {...props} />,
+  strong: (props: ComponentPropsWithoutRef<'strong'>) => <strong className="font-bold" {...props} />,
   a: ({ href, children, ...props }: AnchorProps) => {
     const className = 'text-blue-500 hover:text-blue-700';
     if (href?.startsWith('/')) {
@@ -50,13 +36,7 @@ const components = {
       );
     }
     return (
-      <a
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
-        className={className}
-        {...props}
-      >
+      <a href={href} target="_blank" rel="noopener noreferrer" className={className} {...props}>
         {children}
       </a>
     );
